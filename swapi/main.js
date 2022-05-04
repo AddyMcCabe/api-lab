b5caf6ff0fe17491ef29069e96b78e91a55ea7dc
Addison McCabe <addisonhartmccabe@gmail.com>
2022-05-04 14:54:26 -0700

    initial commit

---
+++ swapi/main.js
@@ -0,0 +1,8 @@
+let getResidentsBtn = document.querySelector('#resBtn');
+
+function getResBtn(event) {
+    console.log('button clicked')
+}
+
+getResidentsBtn.addEventListener('click', getResBtn);
+
