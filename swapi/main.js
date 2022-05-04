715cf779cccd2443f4d6054afdd8ea19a200af32
Addison McCabe <addisonhartmccabe@gmail.com>
2022-05-04 16:29:12 -0700

    updated commit

--- swapi/main.js
+++ swapi/main.js
@@ -1,4 +1,4 @@
-let getResidentsBtn = document.querySelector('#resBtn');
+let getResidentsBtn = document.querySelector('#resident-button');
 
 function getResBtn(event) {
     console.log('button clicked')
@@ -6,3 +6,24 @@ function getResBtn(event) {
 
 getResidentsBtn.addEventListener('click', getResBtn);
 
+const getResidents = () => {
+axios.get('https://swapi.dev/api/planets/2')
+    .then(res => {
+        const {residents} = res.data
+
+        residents.forEach(url => {
+            axios.get(url)
+            .then(res => {
+                console.log(res.data)
+
+               let person = document.createElement('h2')
+               person.textContent = res.data.name
+
+               document.getElementById('resident-display').appendChild(person)
+
+            })
+        })
+    })
+}
+
+document.getElementById('resident-button').addEventListener('click', getResidents)
